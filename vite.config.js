049e13c904fd58542fef4914e74c56134dc86c97
Clement <clement@gmail.com>
2025-09-02 12:50:56 -0400

    1.34

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/chepperence/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        order: "order.html",
+      },
+    },
+  },
 });
