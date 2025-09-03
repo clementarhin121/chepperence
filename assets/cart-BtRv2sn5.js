bbac6497601c78221d0dadc8abc7e39fee1d3d83
Clement <clement@gmail.com>
2025-09-03 08:40:11 -0400

    Updates

---
+++ assets/cart-BtRv2sn5.js
@@ -0,0 +1,1 @@
+import{c as r,j as t,r as e}from"./Menu-CLhVztpQ.js";import{C as o}from"./cart-yCrxttGD.js";r.createRoot(document.getElementById("root")).render(t.jsx(e.StrictMode,{children:t.jsx(o,{})}));
